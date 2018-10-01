d4460fdb94aee289ed327a192552f8b50aab3528
tommatty <6637904+tommatty@users.noreply.github.com>
2018-10-01 11:33:01 -0400

    test 2 local

--- app/test.js
+++ app/test.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<script>alert(\'test\');</script>')
-    res.end('Hello World!');
+    res.write('<script>alert(\'testgh2\');</script>')
+    res.end('Worked!');
 }).listen(8080);
